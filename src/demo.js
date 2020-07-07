9963174c6fc09471e69b2d6d82a283548afc88c8
Nicolas <aponcenicolas@gmail.com>
2020-07-07 14:41:36 -0500

    preparando para subir un commit por cada tema visto

--- src/demo.js
+++ src/demo.js
@@ -1,45 +1,17 @@
-import React, { useState } from 'react'
-import { fromEvent } from 'rxjs'
-import { mergeMap, pluck, debounceTime } from 'rxjs/operators'
-import { ajax } from 'rxjs/ajax'
+import React from "react";
+import { Observable } from "rxjs";
+import { map, filter, take } from "rxjs/operators";
+import {} from "rxjs/ajax";
 
 const Demo = () => {
+  
 
-    const[name,setName]=useState('')
-    const onChangeName=({target})=>{
-        setName(target.value)
-    }
+  return (
+    <div>
+      <h1>Hola Mundo Cruel</h1>
+      <hr />
+    </div>
+  );
+};
 
-    const url='http://localhost:9898/api/person/get'
-    const click$=fromEvent(document,'click')
-
-    click$.pipe(
-        debounceTime(5000),
-        mergeMap(()=>ajax({
-            url:url,
-            method:'POST',
-            headers:{
-                'Content-type':'application/json'
-            },
-            body:{name}
-        }).pipe(
-            pluck('response','result')
-        ))
-    ).subscribe(console.log)
-
-    return (
-     <div>
-         <h1>Hello World</h1>
-         <hr/>
-         <input
-            value={name}
-            onChange={onChangeName}
-         />
-         <input
-            placeholder="BuscarPersona"
-         />
-     </div>
-    )
-}
-
-export default Demo
+export default Demo;
---
+++ src/xdxdxd/rxjs.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { map, filter } from "rxjs/operators";
+import { Observable } from "rxjs";
+
+const RxJs = () => {
+  
+  return (
+    <div>
+      <h1>Esto son ejemplos pra no olvidarme de RxJs</h1>
+    </div>
+  );
+};
+
+export default RxJs;
