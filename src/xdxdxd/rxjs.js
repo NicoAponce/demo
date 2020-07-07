b394d476ff80314b3f95f794926dc25530a20db8
Nicolas <aponcenicolas@gmail.com>
2020-07-07 14:42:43 -0500

    ejemplo sobre observable

--- src/xdxdxd/rxjs.js
+++ src/xdxdxd/rxjs.js
@@ -3,6 +3,31 @@ import { map, filter } from "rxjs/operators";
 import { Observable } from "rxjs";
 
 const RxJs = () => {
+  /**
+   ************************************************
+   *    Ejemplo numero uno sobre observable.
+   ************************************************
+   **/
+  const observer = {
+    next: (val) => console.log("valor Next: ", val),
+    error: (err) => console.log("valor error: ", err),
+    complete: () => console.log("fin del mundo"),
+  };
+
+  const obs$ = new Observable((obs) => {
+    obs.next(1);
+    obs.next(3);
+    obs.next(2);
+    obs.next(5);
+  });
+
+  obs$
+    .pipe(
+      map((val) => val * 15),
+      filter((val) => val > 20)
+    )
+    .subscribe(observer);
+
   
   return (
     <div>
